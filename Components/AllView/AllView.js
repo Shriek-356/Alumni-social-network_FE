abae202b0d940155870b82d884e62c731f54d448
Theanh130124 <theanhtran13012004@gmail.com>
2025-01-24 10:36:37 +0700

    Update Header

--- Components/AllView/AllView.js
+++ Components/AllView/AllView.js
@@ -1,18 +1,39 @@
-
-import { createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from '../Home/Home';
 import Account from '../Home/Account';
+import Icon from 'react-native-vector-icons/Ionicons';
+import React from 'react';
+import { View } from 'react-native';
+import Header from '../Layout/Header';
 
+const Tab = createBottomTabNavigator();
 
-const tab = createBottomTabNavigator();
-
-export default AllView = () =>{
-    return(
-            <tab.Navigator  >
-                <tab.Screen name="Home" component={Home} />
-                <tab.Screen name="Account" component={Account} />
-            </tab.Navigator>
-    )
+export default function AllView() {
+    return (
+        
+        <Tab.Navigator screenOptions={{
+                    header: () => <Header />, // Thêm Header cho tất cả các tab
+                }}>
+            <Tab.Screen 
+                name="Home" 
+                component={Home}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="home" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Tab.Screen 
+                name="Account" 
+                component={Account}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="person" size={size} color={color} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+        
+    );
 }
-
--- Components/Home/Account.js
+++ Components/Home/Account.js
@@ -1,12 +1,70 @@
+import { useContext, useEffect } from "react"
 import styles from "../../styles/styles"
 import { View,Text } from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getAccountUser, getCurrentUser } from "../../configs/API/userApi";
+import Styles from "./Styles";
+import { useState } from "react";
 
-const Account = () =>{
+const Account = ({navigation}) =>{
+    
+    const[userInfo,setUserInfo] = useState({});
+    
+    
+    
+
+    const getCurrentUser = async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            let res = await djangoAuthApi(token).get(endpoints['getAccountUser'](user.id));
+            setUserInfo(res.data);
+               
+            
+        } catch (err) {
+            console.log(err)
+        }
+    };
+    const getAccountbyUser = async () => {
+        try{
+            const token = await AsyncStorage.getItem("token");
+            const userId = userInfo.id;
+            if(token && userId){
+                const accountData = await getAccountUser(token,userId)
+                console.log("Account Data:", accountData);
+                setUserInfo((prev) =>({...prev,
+                    avatar:accountData.avatar,
+                    cover_avatar:accountData.cover_avatar,
+                    phone_number:accountData.phone_number,
+                    date_of_birth:accountData.date_of_birth,
+                    gender:accountData.gender,
+                }));
+            }
+        }
+        catch(err){
+            console.log(err)
+
+        }
+    }
+    
+    useEffect(() => {
+        getCurrentUser();
+    }, [userInfo])
+    useEffect(()=>{
+        if(userInfo.id){
+        getAccountbyUser();}
+    },[userInfo.id]);
     return(
-        <View style={styles.container}>
-            <Text>Account View</Text>
+        <View style={Styles.container_acc}>
+            {userInfo.cover_avatar && <Image source = {{uri: userInfo.cover_avatar}} style={Styles.coverAvatar}/>}
+            {userInfo.avatar && <Image source= {{uri:userInfo.avatar}} style={Styles.avatar}/>}
+            <Text>Họ tên: {userInfo.full_name} {userInfo.last_name}</Text>
+            <Text>Ngày sinh: {userInfo.date_of_birth}</Text>
+            <Text>Giới tính: {userInfo.gender}</Text>
+            <Text>Số điện thoại: {userInfo.phone_number}</Text>
+            <Text>Roke</Text>
         </View>
     )
-}
+};
 
-export default Account
+
+export default Account;
